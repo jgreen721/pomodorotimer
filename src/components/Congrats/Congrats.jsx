577d073995e58ad481dd4cb138083d40dca8cb1e
Justin Green <justingreen@Justins-MacBook-Air.local>
2023-07-16 09:04:55 -0700

    save

--- src/components/Congrats/Congrats.jsx
+++ src/components/Congrats/Congrats.jsx
@@ -7,6 +7,9 @@ const Congrats = () => {
   const {time} = useAppContext();
   return (
       <>
+      <h1>STRONG</h1>
+      <h1>WORK</h1>
+      <h1>💪</h1>
     {time.curr == 0 && <Confetti width={innerWidth} height={innerHeight}/>}
     </>
     )
--- src/components/Selector/Selector.jsx
+++ src/components/Selector/Selector.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react'
+import React, {useRef,useState} from 'react'
 import SelectItem from './components/SelectItem'
 import { useAppContext } from '../../context/AppContext'
 import "./Selector.css"
@@ -10,15 +10,23 @@ const Selector = () => {
         {id:3,name:"long break",type:"long",isSelected:false},
     ]
     const highlightRef = useRef();
-    const {updateSelectedTime} = useAppContext()
+    const {updateSelectedTime,isRunning,color} = useAppContext()
+    const [error,setError] = useState("")
 
 
     const selectOption=(timeObj)=>{
+      if(error.length)return;
+      if(isRunning)return togglePause()
         let id = timeObj.id - 1;
         let percentMove = Math.floor(id * 33)
              console.log('PercentMove: ' + percentMove + "%") 
              highlightRef.current.style.left = `${percentMove}%`
         updateSelectedTime(timeObj.name);
+    }
+
+    const togglePause=()=>{
+      setError("Must pause/restart timer to change!🥺")
+      setTimeout(()=>setError(""),2000);
     }
   return (
     <section className="selector-container">
@@ -30,6 +38,7 @@ const Selector = () => {
            <SelectItem key={time.id} time={time} selectOption={selectOption}/>
             ))}
         </ul>
+        <h3 className={`error-h3 ${color}-text`}>{error}</h3>
     </section>
   )
 }
